6f552b8a2f86beef59250df921d15d519224a6ee
marybaxmann <baxmannmariajose@gmail.com>
2025-11-18 15:33:11 -0300

    front

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 // usa localhost en vez de 127.0.0.1
-const API_URL = "http://localhost:8000";
+const API_URL = "http://localhost:8080";
 
 // ⚡ Crear instancia de axios con tamaño máximo aumentado
 const api = axios.create({
