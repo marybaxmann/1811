4053de304c47b8c93a15af4d479853f1ec701f81
marybaxmann <baxmannmariajose@gmail.com>
2025-12-02 01:17:43 -0300

    Versión inicial del proyecto lista para Railway

--- backend/app/main.py
+++ backend/app/main.py
@@ -88,4 +88,5 @@ def spa(full_path: str):
 # ============================
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run("main:app", host="127.0.0.1", port=8000, reload=True)
+    uvicorn.run("app.main:app", host="127.0.0.1", port=8000, reload=True)
+
